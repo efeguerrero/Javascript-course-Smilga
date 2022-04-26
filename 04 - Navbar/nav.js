eeb9b33f0315925ae46d3ad84296afcf9646cc23
Francisco Guerrero <juanfg.8640@gmail.com>
2022-04-25 23:33:06 -0300

    short toggle code added

--- 04 - Navbar/nav.js
+++ 04 - Navbar/nav.js
@@ -1,10 +1,18 @@
 const navToggle = document.querySelector('.navIcon');
 const linkContainer = document.querySelector('.linksContainer');
 
+//Short code to toggle navNabar
+
 navToggle.addEventListener('click', function () {
-  if (linkContainer.classList.contains('linksContainer-expand')) {
-    linkContainer.classList.remove('linksContainer-expand');
-  } else {
-    linkContainer.classList.add('linksContainer-expand');
-  }
+  linkContainer.classList.toggle('linksContainer-expand');
 });
+
+//Long code to toggle navNabar
+
+// navToggle.addEventListener('click', function () {
+//   if (linkContainer.classList.contains('linksContainer-expand')) {
+//     linkContainer.classList.remove('linksContainer-expand');
+//   } else {
+//     linkContainer.classList.add('linksContainer-expand');
+//   }
+// });
