d6a1c2d4c67dba354fa5e85cd8719e2df0ea48c6
Francisco Guerrero <juanfg.8640@gmail.com>
2022-05-07 20:42:35 -0300

    Added option to delete item by item

--- 14 - Grocery List/app.js
+++ 14 - Grocery List/app.js
@@ -1,7 +1,4 @@
-let listItems = [
-  { id: 1, name: 'eggs' },
-  { id: 2, name: 'bacon' },
-];
+let listItems = [];
 
 let insertArray = [];
 
@@ -21,8 +18,8 @@ form.addEventListener('submit', function (e) {
   const itemObj = { id: idCounter, name: nameInput.value }; //Before pushing object inside array we must first creat it//
   listItems.push(itemObj);
   // Populate insertArray to insert HTML
-  insertArray.push(`<li class="item">
-      <h2 class="itemTitle">${nameInput.value}</h2>
+  insertArray.push(`<li class="item" id="${idCounter}">
+      <h2 class="itemTitle" >${nameInput.value}</h2>
       <div class="itemIcons">
       <img src="./icons/edit.svg" class="icon editIcon" alt="" />
       <img src="./icons/trash-2.svg" class="icon deleteIcon" alt="" />
@@ -34,13 +31,42 @@ form.addEventListener('submit', function (e) {
 });
 
 ///////////////////////////
-//COMPETE LIST CLEARING7///
+//COMPLETE LIST CLEARING7///
 //////////////////////////
 
 const clearItems = document.querySelector('.clearItems');
 
 clearItems.addEventListener('click', function () {
+  listItems = [''];
   insertArray = [''];
   itemList.innerHTML = insertArray.join('');
   idCounter = 1;
+});
+
+//////////////////////////
+//DELETING ITEMS/////////
+////////////////////////
+
+//Traverse the DOM to get the id of elements inserted dynamically when I click on delete Icon
+
+itemList.addEventListener('click', function (e) {
+  if (e.target.classList.contains('deleteIcon')) {
+    listItems = listItems.filter(function (item) {
+      return item.id != e.target.parentElement.parentElement.id; //filter listItem array leaving out element with ID clicked
+    });
+  }
+
+  // map new insertArray with new filtered listItems
+
+  insertArray = listItems.map(function (item) {
+    const code = `<li class="item" id="${item.id}">
+      <h2 class="itemTitle" >${item.name}</h2>
+      <div class="itemIcons">
+      <img src="./icons/edit.svg" class="icon editIcon" alt="" />
+      <img src="./icons/trash-2.svg" class="icon deleteIcon" alt="" />
+      </div></li>`;
+    return code;
+  });
+
+  itemList.innerHTML = insertArray.join('');
 });
