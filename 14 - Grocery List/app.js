b523953d0b6adb008e5924feb87baf08bf1ddfe8
Francisco Guerrero <juanfg.8640@gmail.com>
2022-05-08 00:05:46 -0300

    empty item fixed - alert messages with timer - empty list message

--- 14 - Grocery List/app.js
+++ 14 - Grocery List/app.js
@@ -2,6 +2,8 @@ let listItems = [];
 
 let insertArray = [];
 
+let timer;
+
 ///////////////////
 //ITEM INSERTION///
 ///////////////////
@@ -10,37 +12,55 @@ const nameInput = document.querySelector('.listInput');
 const submitBtn = document.querySelector('.inputSubmit');
 const form = document.querySelector('.listForm');
 const itemList = document.querySelector('.itemList');
+const mainTitle = document.querySelector('.mainTitle');
+const alertContainer = document.querySelector('.alertContainer');
 let idCounter = 1;
 
 form.addEventListener('submit', function (e) {
   //Add items to listItems Array
   e.preventDefault();
-  const itemObj = { id: idCounter, name: nameInput.value }; //Before pushing object inside array we must first creat it//
-  listItems.push(itemObj);
-  // Populate insertArray to insert HTML
-  insertArray.push(`<li class="item" id="${idCounter}">
+  if (nameInput.value) {
+    const itemObj = { id: idCounter, name: nameInput.value }; //Before pushing object inside array we must first creat it//
+    listItems.push(itemObj);
+    // Populate insertArray to insert HTML
+    insertArray.push(`<li class="item" id="${idCounter}">
       <h2 class="itemTitle" >${nameInput.value}</h2>
       <div class="itemIcons">
       <img src="./icons/edit.svg" class="icon editIcon" alt="" />
       <img src="./icons/trash-2.svg" class="icon deleteIcon" alt="" />
       </div></li>`);
-  itemList.innerHTML = insertArray.join('');
-  console.log(idCounter);
-  idCounter++;
-  nameInput.value = ''; //go back to placeholder after entering item
+    itemList.innerHTML = insertArray.join('');
+    idCounter++;
+    nameInput.value = ''; //go back to placeholder after entering item
+    alertContainer.innerHTML = `<h3 class="actionmsg add">Item Added</h3>`;
+    clearTimeout(timer);
+    timer = setTimeout(removeAlert, 2000);
+    //set timer as global variable then reset it everytime before execution settimeout function. So that time goes back to maximum on each click
+    if (itemList.textContent) {
+      clearbtn.innerHTML = `<h3 class="clearItems">Clear Items</h3>`;
+    } else clearbtn.innerHTML = '';
+  } else {
+    alertContainer.innerHTML = `<h3 class="actionmsg delete">Please Enter Item</h3>`;
+    clearTimeout(timer);
+    timer = setTimeout(removeAlert, 2000);
+  }
 });
 
 ///////////////////////////
 //COMPLETE LIST CLEARING7///
 //////////////////////////
 
-const clearItems = document.querySelector('.clearItems');
+const clearbtn = document.querySelector('.clearbtn');
 
-clearItems.addEventListener('click', function () {
-  listItems = [''];
-  insertArray = [''];
+clearbtn.addEventListener('click', function () {
+  listItems = [];
+  insertArray = [];
   itemList.innerHTML = insertArray.join('');
   idCounter = 1;
+  alertContainer.innerHTML = `<h3 class="actionmsg delete">List Cleared</h3>`;
+  clearTimeout(timer);
+  timer = setTimeout(removeAlert, 2000);
+  clearbtn.innerHTML = '';
 });
 
 //////////////////////////
@@ -58,6 +78,8 @@ itemList.addEventListener('click', function (e) {
 
   // map new insertArray with new filtered listItems
 
+  console.log(listItems);
+
   insertArray = listItems.map(function (item) {
     const code = `<li class="item" id="${item.id}">
       <h2 class="itemTitle" >${item.name}</h2>
@@ -69,4 +91,36 @@ itemList.addEventListener('click', function (e) {
   });
 
   itemList.innerHTML = insertArray.join('');
+  alertContainer.innerHTML = `<h3 class="actionmsg delete">Item Deleted</h3>`;
+  clearTimeout(timer);
+  timer = setTimeout(removeAlert, 2000);
+  if (itemList.textContent) {
+    clearbtn.innerHTML = `<h3 class="clearItems">Clear Items</h3>`;
+  } else {
+    clearbtn.innerHTML = '';
+    itemList.innerHTML = ` <span class="emptyList">List Empty</span>`;
+  }
+});
+
+/////////////////////////
+//REMOVE ALERT FUNCTION//
+/////////////////////////
+
+function removeAlert() {
+  alertContainer.innerHTML = '';
+}
+
+/////////////////////////////////////////
+//CLEAR ITEMS SHOW ON/OFF ON PAGE LOAD //
+////////////////////////////////////////
+
+//when doc loads, check if list is empty to decide if inserting code
+
+window.addEventListener('load', function () {
+  if (itemList.textContent) {
+    clearbtn.innerHTML = `<h3 class="clearItems">Clear Items</h3>`;
+  } else {
+    clearbtn.innerHTML = '';
+    itemList.innerHTML = ` <span class="emptyList">List Empty</span>`;
+  }
 });
